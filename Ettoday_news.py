30f594749abb9fc5b655b85e9dce85baf45ab52a
U10416020 <wkps19961015@gmail.com>
2018-05-03 17:34:02 +0800

    Crawl website and store data in database
    Crawl the title, content, keywords, and date.
    And use jieba to select keywords from content.
    Then, store the data into the database.

--- Ettoday_news.py
+++ Ettoday_news.py
@@ -1,122 +1,83 @@
 
 # coding: utf-8
 
-# In[18]:
-
-
 import requests
 import lxml
 from bs4 import BeautifulSoup
 import MySQLdb
 import jieba.posseg as pseg
+import jieba
 
 def getNews(url):
     res = requests.get(url)
-    #print res.text
-    #soup = BeautifulSoup(res.text,"html.parser")
     soup = BeautifulSoup(res.text,"lxml")
 
     title = soup.title.string
-    print "Title: "+title
+    #print "Title: "+title
 
-    date = soup.find("time").text
-    print date
-    #keywords = soup.find("meta",attrs={'name':'news_keywords'})
-    #print keywords['content']
+    date = soup.find("time").text.strip()
+    #print date
+    
+    kinds = soup.find(attrs={"name":"section"})['content']
+    #print "Kinds: "+kinds
+    
     keywords = soup.find(attrs={"name":"news_keywords"})['content']  
-    print keywords
+    
+    while keywords.endswith(","):
+        keywords = keywords[:-1]
+    keywords = keywords.split(",")
+    #print keywords
+    
+    f = open('Test.txt','w')
+    for key in keywords:
+        key = key.encode('utf-8')
+        f.write(key+" 20 n\n")
+    f.close() 
+    
     
     description = soup.find("meta",property="og:description")
-    print "Description: "+description["content"]
+    #print "Description: "+description["content"]
     
     content = soup.find("div","story").find_all("p")    
     text=""
     for i in content:
         text=text+i.text
-    print "Content: "+text
+    #print "Content: "+text
+    
+    jieba.load_userdict("Test.txt")
     words = pseg.cut(text)
+    keyword_string=""
     for word in words:
-        if word.flag.find('n') !=-1:
-            print word.word, word.flag
+        if word.flag == 'n'and keyword_string.find(word.word)==-1:
+            keyword_string+=word.word+","
+            #print word.word, word.flag 
+    keyword_string=keyword_string[:-1]
+    #print keyword_string
+            
+    db = MySQLdb.connect(host="",user="", passwd="",db="testdb", charset="utf8")
+    cursor = db.cursor()
     
-jieba.set_dictionary('dict.txt.big')
-res = requests.get("https://www.ettoday.net/news/hot-news.htm")
-soup = BeautifulSoup(res.text,"lxml")
-#url = soup.find_all("div","piece clearfix")
-#url = soup.find_all("h3")
-url = soup.find_all('a',"pic")
-url[50:]=()
-#print "URL!!!"
-#print url
-for i in url:
-    print "content:"+i.text
-    href=i.get("href")
-    print "HREF:"+href
-    getNews("https://www.ettoday.net/"+href)
+    repeat = "SELECT * FROM news where link='%s';"%(url)
+    cursor.execute(repeat)
+    if not cursor.rowcount:        
+        sql = "INSERT INTO news(Link, Title, Content, Kinds, Post_time, Keywords)                VALUES ('%s', '%s', '%s', '%s', '%s', '%s' )" %                (url, title, text , kinds, date, keyword_string)
+        # 插入資料
+        cursor.execute(sql)
+        db.commit()
 
-
-# In[31]:
-
-
-import requests
-import lxml
-import jieba
-import jieba.posseg as pseg
-#import jieba.analyse
-from bs4 import BeautifulSoup
-jieba.set_dictionary('dict.txt.big')
-jieba.load_userdict("Test.txt")
-#res = requests.get("https://star.ettoday.net/news/1130434")
-#res = requests.get("https://www.ettoday.net/news/20180315/1130644.htm")
-res = requests.get("https://www.ettoday.net/news/20180409/1147040.htm")
-#res = requests.get("https://www.ettoday.net/news/20180409/1146992.htm")
-
-soup = BeautifulSoup(res.text,"lxml")
-content = soup.find("div","story").text.strip() 
-#print "Content: "+content
-#print soup
-date = soup.find("time").text
-print date
-#keywords = soup.find("meta",attrs={'name':'news_keywords'})
-#keywords = keywords['content']
-keywords = soup.find(attrs={"name":"news_keywords"})['content']
-while keywords.endswith(","):
-    keywords = keywords[:-1]
-keywords = keywords.split(",")
-print keywords
-
-f = open('Test.txt','a')
-for key in keywords:
-    key = key.encode('utf-8')
-    f.write(key+" 20 n\n")
-f.close()
-
-description = soup.find("meta",property="og:description")
-description = description["content"]
-print "Description: "+description
-
-content = soup.find("div","story").find_all("p")    
-text=""
-for i in content:
-    text=text+i.text
-print "Content: "+text
-
-
-words = jieba.cut(description)
-for word in words:
-    print word
+    db.close()
     
-words = pseg.cut(text)
-for word in words:
-    if word.flag.find('n') !=-1:
-        print word.word, word.flag
+if __name__ == '__main__':     
+    jieba.set_dictionary('dict.txt.big')
+    ettoday_url="https://www.ettoday.net"
+    res = requests.get("https://www.ettoday.net/news/hot-news.htm")
+    soup = BeautifulSoup(res.text,"lxml")
+    url = soup.find_all('a',"pic")
+    url[50:]=()
 
-#tags = jieba.analyse.extract_tags(text, 10)
-#print "Output："
-#print ",".join(tags)
-
-
-# In[ ]:
-
-
+    for i in url:
+        #print "content:"+i.text
+        href=i.get("href")
+        #print "HREF:"ettoday_url+href
+        getNews(ettoday_url+href)
 
--- Set_News.py
+++ Set_News.py
@@ -1,38 +1,67 @@
 
 # coding: utf-8
 
-# In[17]:
-
 import requests
 import lxml
 from bs4 import BeautifulSoup
 import MySQLdb
 from itertools import izip
+import jieba.posseg as pseg
+import jieba
 
 def getNews(url,kind):
     res = requests.get(url)
     #print res.text
     #soup = BeautifulSoup(res.text,"html.parser")
     soup = BeautifulSoup(res.text,"lxml")
-
+    #print "URL:"+url
     #title = soup.title.string    
     if kind == u'娛樂' or kind == u'日韓' or kind == u'華流':
-        title = soup.find(id="newsTitle").text
-        date = soup.find("div","time").text
+        title = soup.find(id="newsTitle").text.strip()
+        date = soup.find("div","time").text.strip()
         content = soup.find("div","Content2").text.strip()
+        keywords = soup.find(attrs={"name":"keywords"})['content'].strip()  
     else:
         title = soup.find("div","title").text.strip()
-        date = soup.find("span","date").text
+        date = soup.find("span","date").text.strip()
         content = soup.find("div",id="Content1").text.strip() 
-        
-        
-    print "Title: "+title
+        keywords = soup.find(attrs={"name":"Keywords"})['content'].strip()
     
-    print "Date: "+date
-    description = soup.find("meta",property="og:description")
-    print "Description: "+description["content"]
+    while keywords.endswith(","):
+        keywords = keywords[:-1]
+    keywords = keywords.split(",")
     
-    print "Content: "+content
+    f = open('Test.txt','w')
+    for key in keywords:
+        key = key.encode('utf-8')
+        f.write(key+" 20 n\n")
+    f.close() 
+    
+    jieba.load_userdict("Test.txt")
+    words = pseg.cut(content)
+    keyword_string=""
+    for word in words:
+        if word.flag == 'n'and keyword_string.find(word.word)==-1:
+            keyword_string+=word.word+","
+            #print word.word, word.flag 
+    keyword_string=keyword_string[:-1]
+    if content.find("'") != -1:
+    	content_string = content.split("'")
+    	content=""
+    	for con in content_string:
+        	content+=con 
+    db = MySQLdb.connect(host="",user="", passwd="",db="testdb", charset="utf8")
+    cursor = db.cursor()
+    
+    repeat = "SELECT * FROM news where link='%s';"%(url)
+    cursor.execute(repeat)
+    if not cursor.rowcount:        
+        sql = "INSERT INTO news(Link, Title, Content, Kinds, Post_time, Keywords)                VALUES ('%s', '%s', '%s', '%s', '%s', '%s' )" %                (url, title, content , kind, date, keyword_string)
+        # 插入資料
+        cursor.execute(sql)
+        db.commit()
+
+    db.close()
 
 def getUrl(page):    
     res = requests.get(page)
@@ -46,43 +75,18 @@ def getUrl(page):
     url[30:]=()
     for i,j in izip(url,kind_all):
         title = i.text
-        print "Title:"+title
+        #print "Title:"+title
         kind = j.text
-        print "Kind: "+kind       
+        #print "Kind: "+kind       
         href=i.get("href")
-        print "HREF:"+href
+        #print "HREF:"+href
         getNews("http://www.setn.com"+href,kind) 
        
     if page!= lastPage:
         getUrl("http://www.setn.com"+changePage)
 
-hotNews = "http://www.setn.com/ViewAll.aspx?PageGroupID=0"
-getUrl(hotNews)
-
-
-# In[17]:
-
-import requests
-import lxml
-from bs4 import BeautifulSoup
-res = requests.get("http://www.setn.com/News.aspx?NewsID=357539")
-soup = BeautifulSoup(res.text,"lxml")
-#description = soup.find("meta",property="og:description")
-#print "Description: "+description["content"]
-description = soup.find(attrs={"name":"Description"})['content']  
-print "Desciprtion : "+description
-keywords = soup.find(attrs={"name":"Keywords"})['content']  
-print keywords
-
-print soup
-
-
-# In[5]:
-
-
-
-
-# In[ ]:
-
-
+if __name__ == '__main__':  
+    jieba.set_dictionary('dict.txt.big')
+    hotNews = "http://www.setn.com/ViewAll.aspx?PageGroupID=0"
+    getUrl(hotNews)
 
